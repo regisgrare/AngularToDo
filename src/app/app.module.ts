a8f80b969686eb9a6f580ebf0054b6f4e9302988
Régis GRARE <regisgrare@hotmail.fr>
2022-05-23 17:06:21 +0200

    drag N drop OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, TodoComponent],
@@ -27,6 +28,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,11 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ITask } from '../model/task';
 
 @Component({
   selector: 'app-todo',
@@ -8,11 +14,36 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class TodoComponent implements OnInit {
   todoForm!: FormGroup;
+  tasks: ITask[] = [];
+  inprogress: ITask[] = [];
+  done: ITask[] = [];
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.todoForm = this.fb.group({
       item: ['', Validators.required],
     });
+  }
+  addTask() {
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done: false,
+    });
+  }
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
